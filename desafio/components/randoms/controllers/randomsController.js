aaae25c76a0cd936dd2988d3f49eb8a15f5f3981
e-chevalier <echevalier.dev@gmail.com>
2022-04-29 19:52:10 -0300

    Remove children_process from randoms endpoint

--- desafio/components/randoms/controllers/randomsController.js
+++ desafio/components/randoms/controllers/randomsController.js
@@ -4,17 +4,20 @@ import logger from '../../../utils/log4js/log4js_config.js'
 class Randoms {
     async getRandoms(req, res, next) {
         try {
-            let forked = await randomsService.getRandoms(req)
+            // let forked = await randomsService.getRandoms(req)
 
-            forked.on('message', msg => {
-                if(msg == 'ready'){
-                    logger.info(msg)
-                    forked.send("Esperando resultado.")
-                } else {
-                    //res.setHeader('Cache-Control', 'no-transform')
-                    res.render('randoms', {data: msg, cant: req.query.cant || 100000000} )
-                }
-            })
+            // forked.on('message', msg => {
+            //     if(msg == 'ready'){
+            //         logger.info(msg)
+            //         forked.send("Esperando resultado.")
+            //     } else {
+            //         //res.setHeader('Cache-Control', 'no-transform')
+            //         res.render('randoms', {data: msg, cant: req.query.cant || 100000000} )
+            //     }
+            // })
+            
+            let response = await randomsService.getRandoms(req)
+            res.render('randoms', {data: response, cant: req.query.cant || 100000000} )
 
         } catch (error) {
             logger.error(error)
